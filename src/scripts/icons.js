0fdb7d46a4fa5c9baf0926b293a45aff03b3f235
Shannon <69023504+Cedrus32@users.noreply.github.com>
2022-11-15 17:21:49 -0600

    Initial commit

---
+++ src/scripts/icons.js
@@ -0,0 +1,23 @@
+// cache images
+let imgQuery = require.context('../icons', false, /\.(png|svg|jpg|jpeg|gif)$/i);
+let iconsArray = importIcons(imgQuery);
+console.log('***** ICONS ARRAY *****');
+console.log(iconsArray);
+
+// methods
+function importIcons(r) {
+    let queryKeys = r.keys();
+    let icons = [];
+    for (let i = 0; i < queryKeys.length; i++) {
+        let newKey = queryKeys[i].split('.svg')[0].split('./')[1];
+        let newValue = `./icons/${queryKeys[i].split('./')[1]}`;
+        //// console.log(`newKey --> ${newKey}`);
+        //// console.log(`newValue --> ${newValue}`)
+        
+        icons[newKey] = newValue;
+    };
+
+    return icons;
+}
+
+export default iconsArray;
