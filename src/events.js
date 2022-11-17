1e308f4e41b8325a14af0398298bdadcfdb32184
Shannon <shannon@Shannons-MacBook-Pro.local>
2022-11-17 15:28:19 -0600

    Add pubsub module

---
+++ src/events.js
@@ -0,0 +1,45 @@
+const events = (() => {
+    let _events = {};
+
+    // subscribe event to list
+    function subscribe(eventName, callback) {
+        if (!Array.isArray(_events[eventName])) {
+            _events[eventName] = [];
+        };
+        _events[eventName].push(callback);
+        // console.log(`SUBSCRIBING to ${eventName}`);
+    }
+
+    // unsubscribe event to list
+    function unsubscribe(eventName) {
+        console.log(eventName);
+        console.log(_events[eventName].length - 1);
+        _events[eventName].splice((_events[eventName].length - 1), 1);
+    }
+
+    // publish event with data
+    function publish(eventName, ...data) {
+        if (!Array.isArray(_events[eventName])){
+            return;
+        }
+        _events[eventName].forEach(callback => {
+            callback(...data);
+            // console.log(`PUBLISHING to ${eventName}`);
+        });
+    }
+
+    // debug
+    function viewEvents() {
+        console.log(_events);
+    }
+
+    return {
+        subscribe,
+        unsubscribe,
+        publish,
+        viewEvents,
+    }
+
+})();
+
+export default events;
